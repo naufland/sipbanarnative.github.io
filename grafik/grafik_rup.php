6bae3c740481a0ab6dab3b58de78e28b2de00b68
naufland <itsraven76@gmail.com>
2025-09-18 12:07:10 +0800

    add menu grafik

---
+++ grafik/grafik_rup.php
@@ -0,0 +1,28 @@
+<?php
+
+include '../navbar/header.php';
+
+
+
+
+
+
+?>
+<!-- Custom CSS untuk halaman ini -->
+<script src="../../js/submenu.js"></script>
+<!-- Bootstrap JS harus dimuat dulu -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<!-- Kemudian submenu script -->
+
+<body>
+    <h1>Grafik</h1>
+    <a>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis amet pariatur quam non animi. Natus placeat, repellendus facilis asperiores nisi recusandae ad atque provident deleniti quas ullam dignissimos voluptate architecto.</a>
+</body>
+</body>
+
+
+
+<?php
+// Include footer
+include '../navbar/footer.php';
+?>
